f394b9aa1b652fc8ca8c4417228e0cd0655b15a8
ritesh-bilip <dasritesh537@gmail.com>
2026-02-03 15:05:28 +0530

    Fix rentals app path for double-nested project

--- rental_system/rental_system/settings.py
+++ rental_system/rental_system/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rentals',
+    'rental_system.rentals',
+
 ]
 
 MIDDLEWARE = [
--- rental_system/rental_system/urls.py
+++ rental_system/rental_system/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('rentals.urls')),
+    # path('', include('rentals.urls')),
+    path('', include('rental_system.rentals.urls')),
+
 ]
