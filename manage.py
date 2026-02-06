4e4b527662209f674222d36b35bbabc353ef9d4f
ritesh-bilip <dasritesh537@gmail.com>
2026-02-06 16:50:29 +0530

    Fix nested project structure

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rental_system.settings')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rental_system.rental_system.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rental_system.settings')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rental_system.rental_system.settings')
 
     try:
         from django.core.management import execute_from_command_line
